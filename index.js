cf5a6712aedd9ab57133241e68903637917a5f3f
Keyvan Alonso <keyvanalonso@gmail.com>
2022-03-21 22:24:54 -0400

    commit tree d629f9e00c5b08ed94cc49f5d48c8ac56597e90f

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+
+// PART 1
+var customerName = 'bob';
+
+// PART 2
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+// PART 3
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+// PART 4
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+// PART 5
+const leastFavoriteCustomer = 'keyvan'
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'bob';
+}
